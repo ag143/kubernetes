3cc60b777949080a7ef3580660c0832aa42de8fb
ag143 <ag14341@gmail.com>
2022-05-22 10:50:23 +0530

    Create Test.sh

---
+++ Test.sh
@@ -0,0 +1,21 @@
+cat > config <<EOF
+apiVersion: v1
+clusters:
+- cluster:
+    certificate-authority-data:
+    server: ${{secrets.server}}
+  name: cloud_okteto_com
+contexts:
+- context:
+    cluster: cloud_okteto_com
+    namespace: 
+    user:
+  name: cloud_okteto_com
+current-context: cloud_okteto_com
+kind: Config
+preferences: {}
+users:
+- name: 
+  user:
+    token: 
+EOF
